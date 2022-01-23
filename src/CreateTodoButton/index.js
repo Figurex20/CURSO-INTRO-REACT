5ef006f6bde360ec65f32fd44fd7d64e26710c00
FIGUREX <kennethggm12@gmail.com>
2022-01-23 15:29:58 -0600

    I made the news folders and i added the index.js

---
+++ src/CreateTodoButton/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './CreateTodoButton.css';
+
+function CreateTodoButton() {
+	const onClickButton = () => {
+		alert('Aquí se debería abrir el modal');
+	};
+
+	return (
+		<button className="CreateTodoButton" onClick={onClickButton}>
+			+
+		</button>
+	);
+}
+
+export { CreateTodoButton };
